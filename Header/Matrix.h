db2421393d1238f97a0d0af202ff8abb7986aed4
robin <r.losson@student.isartdigital.com>
2023-04-16 10:48:22 -0400

    cleaning the branches merge

--- Header/Matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LIBMATH__MATRIX_H__
-#define __LIBMATH__MATRIX_H__
-
-#include "Matrix/MatrixInternal.h"
-#include "Matrix/Matrix2.h"
-#include "Matrix/Matrix3.h"
-#include "Matrix/Matrix4.h"
-
-#endif // !__LIBMATH__MATRIX_H__
